63f088d38aecd672d1ceb4282e94883b96dac868
严俊东 <1172482914@qq.com>
2020-09-14 14:32:53 +0800

    关于我 页面

---
+++ page-nuxt/pages/about.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="page-detail">
+      <div>
+        <h1 class="title">
+          关于我
+        </h1>
+        <div class="md-style">
+          <p>普普通通的一个人，做普普通通的事。 平平淡淡的一个前端开发，或者我更喜欢说我是个JS开发，虽然工作一直都是与前端相关。 目前专注NodeJS开发，不知道还得走多久，但我知道还需要走很久。 学无止境，成功留给有准备的人！</p><p><i class="fa fa-user" />微信:<a href="//www.yanjd.top">gaga_change</a></p><p><i class="fa fa-user" />QQ:<a href="//www.yanjd.top">1172482914</a></p><p><i class="fa fa-envelope" />邮箱:<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&amp;email=q8zKzMr0yMPKxczO69rahcjExg" style="text-decoration: none;">gaga_change@qq.com</a></p><p>
+            <i class="fa fa-github" />
+            GitHub:<a href="https://github.com/gaga-change" target="_blank" rel="noopener">@gaga-change</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.page-detail {
+  padding-top: 20px;
+  color: #34495e;
+
+  .title {
+    padding-bottom: 0.4rem;
+    font-size: 2.2rem;
+    line-height: 1.3;
+    margin-top: 0;
+    margin-bottom: 0.5em;
+    color: rgba(0, 0, 0, 0.85);
+    font-weight: 500;
+  }
+
+  p {
+    margin: 0.8em 0;
+    line-height: 1.6;
+  }
+
+  a {
+    color: #42b983;
+    font-weight: 600;
+    padding: 0 2px;
+    text-decoration: none;
+  }
+}
+</style>
