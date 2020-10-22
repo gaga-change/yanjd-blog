6a11a26e6942b02f634beb1b1b67c674c3cc7434
严俊东 <1172482914@qq.com>
2020-10-22 11:00:28 +0800

    标签增删改查api使用 graphql

--- admin-vue/src/api/tags.js
+++ admin-vue/src/api/tags.js
@@ -1,40 +1,83 @@
 import strapi from '@/utils/strapi'
+import gql from 'graphql-tag'
 
 export function tagCreate(data) {
-  return strapi({
-    url: '/tags',
-    method: 'post',
-    data
+  return strapi.post('/graphql', {
+    variables: { tag: data },
+    query: gql`
+      mutation ($tag: TagInput) {
+        createTag(input: {
+          data: $tag
+        }) {
+          tag {
+            id
+          }
+        }
+      }
+    `.loc.source.body
   })
 }
 
 export function tagDelete(id) {
-  return strapi({
-    url: `/tags/${id}`,
-    method: 'delete'
+  return strapi.post('/graphql', {
+    variables: { id },
+    query: gql`
+      mutation ($id: ID! ) {
+        deleteTag(input: {
+          where: { id: $id}
+        }) {
+          tag {
+            id
+          }
+        }
+      }
+    `.loc.source.body
   })
 }
 
 export function tagUpdate(id, data) {
-  return strapi({
-    url: `/tags/${id}`,
-    method: 'put',
-    data
+  return strapi.post('/graphql', {
+    variables: { id, tag: data },
+    query: gql`
+      mutation ($id: ID!, $tag: editTagInput ) {
+        updateTag(input: {
+          where: { id: $id},
+          data: $tag
+        }) {
+          tag {
+            id
+          }
+        }
+      }
+    `.loc.source.body
   })
 }
+
 export async function tagIndex(params) {
-  const dataApi = strapi({
-    url: '/tags',
-    method: 'get',
-    params
+  const { _limit: limit, _start: start, _sort: sort, ...filter } = params
+  return strapi.post('/graphql', {
+    variables: { start, limit, sort, filter },
+    query: gql`
+      query ($start: Int, $limit: Int, $sort: String, $filter: JSON) {
+        tagsConnection(start: $start, limit: $limit, sort: $sort where: $filter ) {
+          values {
+            id
+            name
+            createdAt
+            updatedAt
+          },
+          aggregate {
+            totalCount
+          }
+        }
+      }
+    `.loc.source.body
+  }).then(res => {
+    console.log()
+    const { values, aggregate } = res.data['tagsConnection']
+    return {
+      list: values,
+      total: aggregate['totalCount']
+    }
   })
-  const totalApi = strapi({
-    url: '/tags/count',
-    method: 'get',
-    params: { ...params, _limit: undefined, _start: undefined }
-  })
-  return {
-    list: await dataApi,
-    total: await totalApi
-  }
 }
