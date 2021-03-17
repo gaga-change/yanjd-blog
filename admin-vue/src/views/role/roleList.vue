270508617c80fa7a6600cadd030e2ffeb47a5241
严俊东 <1172482914@qq.com>
2021-03-17 09:32:42 +0800

    角色创建人修改人未显示修复

--- admin-vue/src/views/role/roleList.vue
+++ admin-vue/src/views/role/roleList.vue
@@ -45,9 +45,9 @@ export default {
       { label: '名称', prop: 'name' },
       { label: '描述', prop: 'remark' },
       { label: '创建时间', prop: 'createdAt', type: 'time', width: 140, sortable: 'custom' },
-      { label: '创建人', prop: 'createdBy.name' },
+      { label: '创建人', prop: 'createdBy' },
       { label: '修改时间', prop: 'updatedAt', type: 'time', width: 140, sortable: 'custom' },
-      { label: '修改人', prop: 'updatedBy.name' },
+      { label: '修改人', prop: 'updatedBy' },
       { label: '操作', prop: 'control', type: 'dom', dom: ColModifyAndDel, fixed: 'right' }
     ]
     const searchConfig = [
